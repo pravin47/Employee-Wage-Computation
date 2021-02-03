8d92df0739ad3dba0ab0d594cfbe4ed1eb5dfe8c
pravin47 <bagul.pravin2@gmail.com>
2021-02-03 19:16:56 +0530

    Added:Using_Switch_case

--- Emp_Wage.java
+++ Emp_Wage.java
@@ -7,20 +7,23 @@ public class Emp_Wage {
 		final int Full_Time = 1;
 		final int Part_Time = 2;
 		int empType = (int) (Math.random() * 10) % 3;
-		int Fulldayhour = 0;
-		int Part_Time_Hour = 0;
-		if (empType == Full_Time) {
-			System.out.println("Employee is Present");
-			Fulldayhour = 8;
-		} else if (empType == Part_Time) {
-			System.out.println("employee is part time");
-			Part_Time_Hour = 8;
-		} else {
-			System.out.println("Employee is Absent");
+		int Working_hour = 0;
+
+		switch(empType) {
+		
+		case 1:
+				System.out.println("Employee Is Full Time");
+				Working_hour =8;
+				break;
+		case 2: 
+				System.out.println("Employee is part time");
+				Working_hour = 8;
+				break;
+		default:
+				System.out.println("Employee is Absent");
+				
+			}
+			wage=Wage_Per_Hour*Working_hour;
+			System.out.println("Daily Employee Wage is : " +wage);
 		}
-		wage = Wage_Per_Hour * Fulldayhour;
-		int Parttime_Wage = Wage_Per_Hour * Part_Time_Hour;
-		System.out.println("Employee per Daily Wage is = " + wage);
-		System.out.println("Part time per Daily Wage is = " + Parttime_Wage);
-	}
 }
